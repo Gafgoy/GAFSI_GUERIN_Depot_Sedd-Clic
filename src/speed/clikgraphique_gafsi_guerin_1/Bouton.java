fd6c642810f63a9fce4fd2848ca27e4b69b717a2
hgafs <hgafs@192.168.0.23>
2021-12-30 15:38:17 +0100

    Creation des classes + creation de la classe bouton en console

---
+++ src/speed/clikgraphique_gafsi_guerin_1/Bouton.java
@@ -0,0 +1,44 @@
+package speed.clikgraphique_gafsi_guerin_1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hgafs
+ */
+public class Bouton {
+    
+
+    boolean etreAllume; // definie l'état du bouton
+    //String couleur;
+    //String couleur_vide;
+
+    public Bouton(){ // constructeur de bouton
+
+    etreAllume = false; 
+     //couleur="R";
+     //couleur_vide ="G";
+    }
+
+    public void Alummer_bouton(){// renvoie vrai si le bouton est allumer 
+    etreAllume = true;
+    
+    }
+
+    public void Eteindre_bouton(){ // renvoie vrai si le bouton est eteint
+    etreAllume =false; 
+   
+    
+    }
+    
+    //public String lirecouleurbouton(){
+        //return couleur;
+        
+    //}
+ }
+
+    
+
